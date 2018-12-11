fd95447ddc6450434f4f64e83c7bb568a77cacc8
Adam Dyess <adam.dyess@adtran.com>
2018-12-11 13:19:23 -0600

    cleaned up indexing

--- 2018/day11.py
+++ 2018/day11.py
@@ -1,10 +1,12 @@
 import logging
 from itertools import takewhile, imap
+from collections import namedtuple
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(format='%(asctime)-15s: %(message)s', level=logging.INFO)
 logger.info("Start")
 GRID_SZ = 300
+Cell = namedtuple('Cell', ['sum', 'x', 'y', 'n'])
 
 
 class FuelCellGrid:
@@ -17,22 +19,23 @@ class FuelCellGrid:
         return ((((rack_id * y) + self.ser) * rack_id) / 100) % 10 - 5
 
     def power_nxn_at(self, x, y, n):
-        return sum([self.grid[y + dy][x + dx] for dx in range(-1, n-1) for dy in range(-1, n-1)])
+        return sum([self.grid[y + dy - 1][x + dx - 1] for dx in range(n) for dy in range(n)])
 
     @property
     def grid(self):
         if self._grid is None:
-            self._grid = [[self.power_at(x + 1, y + 1) for x in range(0, GRID_SZ)] for y in
-                          range(0, GRID_SZ)]
+            self._grid = [[self.power_at(x + 1, y + 1)
+                           for x in range(GRID_SZ)]
+                          for y in range(GRID_SZ)]
         return self._grid
 
     def most_power(self, n):
-        all_powers = [(self.power_nxn_at(x, y, n), (x, y))
-                      for x in range(1, GRID_SZ - n + 2)
-                      for y in range(1, GRID_SZ - n + 2)]
+        all_powers = ((self.power_nxn_at(x + 1, y + 1, n), (x + 1, y + 1))
+                      for x in range(GRID_SZ - n + 1)
+                      for y in range(GRID_SZ - n + 1))
         ans = sorted(all_powers)[-1]
         logger.info('Calculating {n} x {n} is {m}'.format(n=n, m=ans))
-        return ans[0], ans[1], n
+        return Cell(ans[0], ans[1][0], ans[1][1], n)
 
 
 assert FuelCellGrid(8).power_at(3, 5) == 4
@@ -45,9 +48,9 @@ assert FuelCellGrid(42).power_nxn_at(21, 61, 3) == 30
 with open('input11.txt') as in_file:
     m = FuelCellGrid(int(in_file.read()))
 
-positive_powers = list(takewhile(lambda i: i[0] > 0, imap(m.most_power, xrange(1, 300))))
-sol1 = positive_powers[2]
-sol2 = sorted(positive_powers)[-1]
+power_levels = list(takewhile(lambda i: i[0] > 0, imap(m.most_power, xrange(1, 300))))
+sol1 = power_levels[2]
+sol2 = sorted(power_levels)[-1]
 
-logger.info('Solution #1: %s,%s', *sol1[1])
-logger.info('Solution #2: %s,%s,%s', sol2[1][0], sol2[1][1], sol2[2])
+logger.info('Solution #1: %s,%s', sol1.x, sol1.y)
+logger.info('Solution #2: %s,%s,%s', sol2.x, sol2.y, sol2.n)

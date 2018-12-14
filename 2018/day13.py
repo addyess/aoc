d6e3bb04df3f9f2f5b7cfb9bf9242362889583fa
Adam Dyess <adam.dyess@adtran.com>
2018-12-13 20:58:51 -0600

    Documented Solution

--- 2018/day13.py
+++ 2018/day13.py
@@ -3,11 +3,23 @@ from collections import namedtuple
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(format='%(asctime)-15s: %(message)s', level=logging.INFO)
-Cart = namedtuple('Cart', ['dir', 'next_turn'])
+Cart = namedtuple('Cart', ['sym', 'next_turn'])
 
 
 class Map:
     DIRECTIONS = '^>v<'
+    POS = {
+        '>': lambda (y, x): (y, x + 1),
+        '<': lambda (y, x): (y, x - 1),
+        'v': lambda (y, x): (y + 1, x),
+        '^': lambda (y, x): (y - 1, x),
+    }
+    SYM = {
+        '>': {'-': '>', '/': '^', '\\': 'v', '+': None},
+        '<': {'-': '<', '/': 'v', '\\': '^', '+': None},
+        'v': {'|': 'v', '/': '<', '\\': '>', '+': None},
+        '^': {'|': '^', '/': '>', '\\': '<', '+': None},
+    }
 
     @classmethod
     def create(cls, txt):
@@ -29,59 +41,36 @@ class Map:
 
     @staticmethod
     def intersect(cart):
+        cur_dir = Map.DIRECTIONS.index(cart.sym)
         next_turn = cart.next_turn
-        next_dir = Map.DIRECTIONS[
-            (Map.DIRECTIONS.index(cart.dir) + next_turn) % len(Map.DIRECTIONS)
-            ]
+        next_dir = Map.DIRECTIONS[(cur_dir + next_turn) % len(Map.DIRECTIONS)]
         return next_dir, ((next_turn - 1) % 3) - 1
 
     def tick(self):
-        vector = {
-            '>': lambda (y, x): (y, x + 1),
-            '<': lambda (y, x): (y, x - 1),
-            'v': lambda (y, x): (y + 1, x),
-            '^': lambda (y, x): (y - 1, x),
-        }
         collisions = []
         carts = self.carts.copy()
-        for (cart_y, cart_x) in sorted(carts.keys()):
-            cart = carts.pop((cart_y, cart_x), None)
-            if not cart:
+        for (cart_y, cart_x) in sorted(carts.keys()):         # Move across x axis, then down y
+            cart = carts.pop((cart_y, cart_x), None)          # Pickup Cart from Map
+            if not cart:                                      # The cart may have collided already
                 continue
 
-            y, x = vector[cart.dir]((cart_y, cart_x))
-            t = self.tracks[y][x]
-
-            if cart.dir == '>':
-                dir = '>' if t == '-' else \
-                    '^' if t == '/' else \
-                        'v' if t == '\\' else None
-            elif cart.dir == '<':
-                dir = '<' if t == '-' else \
-                    'v' if t == '/' else \
-                        '^' if t == '\\' else None
-            elif cart.dir == 'v':
-                dir = 'v' if t == '|' else \
-                    '<' if t == '/' else \
-                        '>' if t == '\\' else None
+            y, x = self.POS[cart.sym]((cart_y, cart_x))       # Next Position for Cart
+            new_sym = self.SYM[cart.sym][self.tracks[y][x]]   # Next Symbol for Cart
+            next_turn = cart.next_turn                        # Next Available Turn for Cart
+            if new_sym is None:
+                new_sym, next_turn = self.intersect(cart)     # Intersection, Apply turn
+            if carts.pop((y, x), None):                       # If there's a cart in new spot
+                collisions.append((x, y))                     # Append Collision Location
             else:
-                dir = '^' if t == '|' else \
-                    '>' if t == '/' else \
-                        '<' if t == '\\' else None
-            dir, next_turn = self.intersect(cart) if dir is None else (dir, cart.next_turn)
-            collides = carts.pop((y, x), None)
-            if collides:
-                collisions.append((x, y))
-            else:
-                carts[y, x] = Cart(dir, next_turn)
-        return Map(self.tracks, carts), collisions
+                carts[y, x] = Cart(new_sym, next_turn)        # Successfully add cart
+        return Map(self.tracks, carts), collisions            # Return new Map and any collisions
 
     def first_collision(self):
         stage = self
         collisions = []
         while not collisions:
             stage, collisions = stage.tick()
-        return collisions
+        return collisions[0]
 
     def remove_collision(self):
         stage = self
@@ -92,43 +81,11 @@ class Map:
 
 
 def main():
-    test = r'''
-/->-\        
-|   |  /----\
-| /-+--+-\  |
-| | |  | v  |
-\-+-/  \-+--/
-  \------/   '''.strip().split('\n')
-    m = Map.create(test)
-    assert m.first_collision() == [(7, 3)]
-
-    test = r'''
-/>-<\  
-|   |  
-| /<+-\
-| | | v
-\>+</ |
-  |   ^
-  \<->/'''.strip().split('\n')
-    m = Map.create(test)
-    assert m.remove_collision() == (6, 4)
-
-    test = r'''
-/><-\  
-|   |  
-| /-+-\
-| | | |
-\-+-/ |
-  |   |
-  \---/'''.strip().split('\n')
-    m = Map.create(test)
-    assert m.first_collision() == [(2, 0)]
-
     logger.info("Start")
     with open('input13.txt') as in_file:
         m = Map.create(in_file.readlines())
-
     sol1, sol2 = m.first_collision(), m.remove_collision()
+    assert sol1 == (5, 102) and sol2 == (46, 45)
     logger.info('Solution #1: %s', sol1)
     logger.info('Solution #2: %s', sol2)
 

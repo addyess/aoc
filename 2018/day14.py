955a189525f869974b1cc01c7a5909794b90f1bf
Adam Dyess <adam.dyess@adtran.com>
2018-12-14 19:22:25 -0600

    refactor, remove bad 'optimizations'

--- 2018/day14.py
+++ 2018/day14.py
@@ -11,44 +11,42 @@ class Score:
 
     def __init__(self, score, elves):
         self.score = list(score)
-        self.score_len = len(score)
         self.elves = elves
 
-    def extend(self, rounds):
+    def _extend(self, rounds):
         elves = self.elves
-        while self.score_len < int(rounds):
+        while len(self.score) < int(rounds):
             cur = self.score[elves[0]], self.score[elves[1]]
             s = sum(cur)
             n1, n2 = (s // 10), s % 10
             self.score.extend([n1, n2] if n1 else [n2])
-            self.score_len += 2 if n1 else 1
-            elves = (elves[0] + cur[0] + 1) % self.score_len, \
-                    (elves[1] + cur[1] + 1) % self.score_len
+            elves = (elves[0] + cur[0] + 1) % len(self.score), \
+                    (elves[1] + cur[1] + 1) % len(self.score)
         self.elves = elves
         return self
 
     def next_ten(self, start):
         start = int(start)
-        if self.score_len < (start+10):
-            self.extend(start+10)
+        if len(self.score) < (start+10):
+            self._extend(start+10)
         return ''.join(map(str, self.score[start:start+10]))
 
     def _find_here(self, m, idx):
         size = len(m)
-        while (idx + size) < self.score_len:
+        while (idx + size) < len(self.score):
             rep = self.score[idx:idx + size]
             if m == rep:
                 return True, idx
             idx += 1
         return False, idx
 
-    def index_of(self, m, idx=0):
+    def index(self, m, idx=0):
         m = map(int, list(str(m)))
         while True:
             found, idx = self._find_here(m, idx)
             if found:
                 return idx
-            self.extend(self.score_len + 10000)
+            self._extend(len(self.score) + 10000)
 
 
 def main():
@@ -65,11 +63,11 @@ def main():
     sol1 = chocolate.next_ten(m)
     logger.info('Solution #1: %s', sol1)
 
-    assert 9 == chocolate.index_of('51589')
-    assert 5 == chocolate.index_of('01245')
-    assert 18 == chocolate.index_of('92510')
-    assert 2018 == chocolate.index_of('59414')
-    sol2 = chocolate.index_of(m)
+    assert 9 == chocolate.index('51589')
+    assert 5 == chocolate.index('01245')
+    assert 18 == chocolate.index('92510')
+    assert 2018 == chocolate.index('59414')
+    sol2 = chocolate.index(m)
     logger.info('Solution #2: %s', sol2)
 
 

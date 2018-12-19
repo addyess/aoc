70b39a16cfc5320d000e9fd8254d538629a554d3
Adam Dyess <adyess@gmail.com>
2018-12-19 11:47:15 -0600

    Day19

---
+++ aoc2018/day19.py
@@ -0,0 +1,63 @@
+from day16 import Registers
+import re
+import logging
+logging.basicConfig(filename='log19.txt', filemode='w',
+                    format='%(asctime)-15s: %(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
+bind = re.compile(r'#ip (\d+)')
+
+
+def execute(instructions, regs):
+    bound_reg, ip = None, 0
+    bound = bind.match(instructions[0])
+    if bound:
+        bound_reg = map(int, bound.groups())[0]
+
+    instructions = instructions[1:]
+    while ip != len(instructions):
+        pre_ip, pre_regs = ip, regs
+        if 0 <= ip <= len(instructions):
+            inst_text = instructions[ip]
+        else:
+            raise StopIteration("ip={} is out of bounds", ip)
+
+        opcode, a, b, c = inst_text.split()
+        regs = regs.execute(opcode, *map(int, (a, b, c)))
+        yield 'ip={}'.format(pre_ip), pre_regs.regs, inst_text, regs.regs
+        if bound_reg is not None:
+            regs = regs.execute('addi', bound_reg, 1, bound_reg)
+            ip = regs.regs[bound_reg]
+        else:
+            ip += 1
+
+
+def emulate(instructions, next_regs):
+    for ip, regs, inst, next_regs in execute(instructions, next_regs):
+        logger.debug('%s %s %s %s', ip, regs, inst, next_regs)
+    return next_regs
+
+
+def sum_factors_of(x):
+    sum = 0
+    for i in range(1, x + 1):
+        if x % i == 0:
+            sum += i
+    return sum
+
+
+def main():
+    logger.info("Start")
+    with open('input19.txt') as in_file:
+        instructions = [l.strip() for l in in_file if l.strip()]
+    regs = emulate(instructions, Registers(0, *[0]*5))
+    logger.info("Solution #1: %d", regs[0])
+
+    for ip, regs, inst, next_regs in execute(instructions, Registers(1, *[0]*5)):
+        if next_regs[2] == 0:
+            break
+
+    logger.info("Solution #2: %d", sum_factors_of(next_regs[1]))
+
+
+if __name__ == '__main__':
+    main()

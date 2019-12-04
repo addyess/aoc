e89cb3a76db06ff9c255b8daf226fe35b05644e1
Adam Dyess <addyess@gmail.com>
2019-12-03 22:42:37 -0600

    clean-up

--- aoc2019/day3.py
+++ aoc2019/day3.py
@@ -1,8 +1,5 @@
-with open("day3.txt") as fin:
-    ins = [
-        _.strip().split(",")
-        for _ in fin.readlines()
-    ]
+def manhattan(point):
+    return abs(point[0]) + abs(point[1])
 
 
 class Wire:
@@ -11,38 +8,39 @@ class Wire:
 
     def intersections(self, wire):
         p = list(self.points.keys() & wire.points.keys())
-        p.sort(key=lambda _: abs(_[0]) + abs(_[1]))
+        p.sort(key=manhattan)
         return p
 
-    def shortest_cross(self, crosspoints, wire):
-        return min([self.points[_] + wire.points[_] for _ in crosspoints])
+    def shortest_cross(self, x_points, wire):
+        return min([self.points[_] + wire.points[_] for _ in x_points])
 
     @classmethod
-    def from_path(cls, path, start=None):
-        x, y = start or (0, 0)
-        inc = {
-            "U": (0, 1), "D": (0, -1), "R": (1, 0), "L": (-1, 0)
-        }
+    def from_path(cls, path):
+        inc = {"U": (0, 1), "D": (0, -1), "R": (1, 0), "L": (-1, 0)}
         points = {}
+        point = 0, 0
         step_count = 0
         for step in path:
             step_dir, step_len = step[0], int(step[1:])
             dx, dy = inc[step_dir]
             for _ in range(step_len):
                 step_count += 1
-                point = x+dx, y+dy
+                point = point[0]+dx, point[1]+dy
                 points[point] = step_count
-                x, y = point
         return cls(points)
 
 
-w1 = Wire.from_path(ins[0])
-w2 = Wire.from_path(ins[1])
+if __name__ == '__main__':
+    with open("day3.txt") as fin:
+        ins = [
+            _.strip().split(",")
+            for _ in fin.readlines()
+        ]
+    w1 = Wire.from_path(ins[0])
+    w2 = Wire.from_path(ins[1])
+    _x_points = w1.intersections(w2)
+    orig_dist = manhattan(_x_points[0])
+    print(f"Result 1: {orig_dist}")
 
-crosspaths = w1.intersections(w2)
-min_x, min_y = crosspaths[0]
-orig_dist = abs(min_x) + abs(min_y)
-print(f"Result 1: {orig_dist}")
-
-comb_path = w1.shortest_cross(crosspaths, w2)
-print(f"Result 2: {comb_path}")
+    comb_path = w1.shortest_cross(_x_points, w2)
+    print(f"Result 2: {comb_path}")

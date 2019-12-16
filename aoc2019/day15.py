2b314eb261ee13c2152905206bff16db618f6312
Adam Dyess <adyess@gmail.com>
2019-12-15 20:57:55 -0600

    Day15 part 1

--- aoc2019/day15.py
+++ aoc2019/day15.py
@@ -13,18 +13,17 @@ class Map:
     SOUTH = (0, 1)
 
     def __init__(self):
-        self.coord = defaultdict(lambda: '?')
+        self.coord = defaultdict(lambda: ('?', float('inf')))
         self.dir = self.EAST
-        self.solution, self.pos, self.prev = None, (0, 0), (None, None)
-        self.coord[self.pos] = '.'
+        self.solution, self.pos = None, (0, 0)
+        self.prev = (None, None), 0
 
     def __iter__(self):
         return self
 
     def __next__(self):
         if self.solution:
-            if len(self.coord) > 1657:
-                return 5
+            return 5
         if self.dir == self.EAST:
             return 4
         elif self.dir == self.WEST:
@@ -38,21 +37,23 @@ class Map:
         return tuple(sum(it) for it in zip(self.pos, _dir))
 
     def send(self, val):
-        ana_pos = self.ana_pos(self.dir)
-        visited = self.coord[ana_pos] != '?'
-        if val == 0:
-            self.coord[ana_pos] = '#'
-        elif val == 1:  # free
-            self.coord[ana_pos] = '.'
-            self.prev = self.pos
-            self.pos = ana_pos
+        cur_pos = self.ana_pos(self.dir)
+        prev_pos, prev_dist = self.prev
+        new_dist = prev_dist + 1
+        cur_val, cur_dist = self.coord[cur_pos]
+        visited = cur_val != '?'
+        moved = val in [1, 2]
+        if val == 0:    # WALL
+            self.coord[cur_pos] = '#', float("inf")
+        elif val == 1:  # FREE
+            self.coord[cur_pos] = '.', min(new_dist, cur_dist)
         elif val == 2:  # FOUND
-            self.coord[ana_pos] = 'F'
-            self.prev = self.pos
-            self.pos = ana_pos
-            self.solution = ana_pos
+            self.coord[cur_pos] = 'F', min(new_dist, cur_dist)
+            self.solution = cur_pos
+        if moved:
+            self.prev = self.pos, min(new_dist, cur_dist)
+            self.pos = cur_pos
         self.turn(visited)
-        return
 
     def turn(self, randomize):
         """Prefer an empty box, then prefer a `right` box with a '#' """
@@ -65,16 +66,16 @@ class Map:
         else:
             rotations = [self.EAST, self.SOUTH, self.WEST, self.NORTH]
         rotations = [(n_dir, self.coord[self.ana_pos(n_dir)]) for n_dir in rotations]
-        rotations = [n_dir for n_dir in rotations if n_dir[1] not in ['#', 'F']]
-        rotations = sorted(rotations, key=lambda n_dir: n_dir[1] != '?')
+        rotations = [n_dir for n_dir in rotations if n_dir[1][0] not in ['#', 'F']]
+        rotations = sorted(rotations, key=lambda n_dir: n_dir[1][0] != '?')
         for n_dir, val in rotations:
-            if val == '?':
+            if val[0] == '?':
                 self.dir = n_dir
                 return
         if randomize and len(rotations) > 2:
             self.dir = rotations[randint(0, len(rotations)-1)][0]
             return
-        rotations = sorted(rotations, key=lambda n_dir: self.ana_pos(n_dir[0]) == self.prev)
+        rotations = sorted(rotations, key=lambda n_dir: self.ana_pos(n_dir[0]) == self.prev[0])
         self.dir = rotations[0][0]
 
     def size(self):
@@ -85,7 +86,8 @@ class Map:
     def render(self):
         minx, miny, maxx, maxy = self.size()
         for y in range(miny-1, maxy+2):
-            line = ''.join(self.coord.get((x, y)) or '?' for x in range(minx-1, maxx+2))
+            line = [self.coord.get((x, y)) or ('?', None) for x in range(minx-1, maxx+2)]
+            line = ''.join([x for x, _ in line])
             if self.pos[1] == y:
                 if self.dir[1] == 0:
                     pointer = '<' if self.dir == self.WEST else '>'
@@ -99,9 +101,7 @@ class Map:
 
     def distance(self):
         self.render()
-        with open('day15.map', 'w') as f:
-            f.write(str(dict(self.coord)))
-        return sum(abs(c) for c in self.solution)
+        return self.coord[self.solution][1]
 
 
 with open("day15.txt") as fin:

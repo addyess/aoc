b3a7e0d1bf8e04e93ad69df737d08999f861e016
Adam Dyess <adyess@gmail.com>
2020-12-01 08:13:57 -0600

    Day01 turned out to be a bust, points were canceled

---
+++ aoc2020/day01.py
@@ -0,0 +1,28 @@
+#! /usr/bin/python3.7
+import math
+
+with open("day01.txt") as f_in:
+    ins = [int(_.strip()) for _ in f_in.readlines()]
+
+
+def find_n_sum(n, series, expected=2020):
+    """
+    Find N integers in a series that sum to the expected value.
+    :param  int n: number of elements to sum
+    :param  list series: list of elements
+    :param  int expected: value to sum to
+    :returns n-tuple of 0s if not found, otherwise n-tuple of the matches
+    """
+    if n == 1:
+        return (expected,) if expected in series else (0,)
+    for cur, i in enumerate(series):
+        res = find_n_sum(n-1, series[cur+1:], expected - i)
+        if not all(_ == 0 for _ in res):
+            return (i,) + res
+    return (0,) * n
+
+
+res1 = find_n_sum(2, ins)
+print(f"Result 1: {math.prod(res1)}")
+res2 = find_n_sum(3, ins)
+print(f"Result 2: {math.prod(res2)}")

c177138bd28cbc67a6addfe18f766843c0d78d23
Adam Dyess <adyess@gmail.com>
2020-12-15 00:22:32 -0600

    Day 15 p2

--- aoc2020/day15.py
+++ aoc2020/day15.py
@@ -3,42 +3,28 @@ with open("day15.txt") as f_in:
     ins = f_in.read().strip()
 
 
-def rindex(_list, value):
-    try:
-        return len(_list) - _list[::-1].index(value) - 1
-    except ValueError:
-        return None
-
-
 class Game:
     def __init__(self, text):
         self.data = [int(_) for _ in text.split(",")]
         self.indexes = {
-            val: idx for idx, val in enumerate(self.data)
+            val: idx for idx, val in enumerate(self.data[:-1])
         }
-
-    def __repr__(self):
-        return ",".join(map(str, self.data))
 
     def item(self, index):
         idx = len(self.data)
         while idx != index:
             last = self.data[-1]
-            prev_idx = self.indexes.get(last)
-            if prev_idx == (idx - 1):
+            prev_idx, self.indexes[last] = self.indexes.get(last), idx - 1
+            if prev_idx is None:
                 add = 0
             else:
                 add = idx - prev_idx - 1
-            prev_idx = rindex(self.data, add)
-            self.indexes[add] = idx if prev_idx is None else prev_idx
             self.data.append(add)
             idx += 1
 
         return self.data[-1]
 
 
-assert Game("0,3,6").item(2020) == 436
-assert Game("1,3,2").item(2020) == 1
 game = Game(ins)
 print(f"Result 1: {game.item(2020)}")
-# assert Game("0,3,6").item(30000000) == 175594
+print(f"Result 2: {game.item(30000000)}")

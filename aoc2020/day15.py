21aac32b179dc4f224b3d7efc96208ae86b94a82
Adam Dyess <adyess@gmail.com>
2020-12-15 00:48:03 -0600

    Simplification of the memory code (day15)

--- aoc2020/day15.py
+++ aoc2020/day15.py
@@ -6,6 +6,8 @@ with open("day15.txt") as f_in:
 class Game:
     def __init__(self, text):
         self.data = [int(_) for _ in text.split(",")]
+        # Record the previous indexes -- except for the last element
+        # We've not evaluated the last element yet
         self.indexes = {
             val: idx for idx, val in enumerate(self.data[:-1])
         }
@@ -13,12 +15,9 @@ class Game:
     def item(self, index):
         idx = len(self.data)
         while idx != index:
-            last = self.data[-1]
-            prev_idx, self.indexes[last] = self.indexes.get(last), idx - 1
-            if prev_idx is None:
-                add = 0
-            else:
-                add = idx - prev_idx - 1
+            this = self.data[-1]
+            prev, self.indexes[this] = self.indexes.get(this), idx - 1
+            add = 0 if prev is None else (idx - prev - 1)
             self.data.append(add)
             idx += 1
 

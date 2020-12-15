a2a47ba8f0d4ebe3982ed6d4ed00b6babc80ea4c
Adam Dyess <adyess@gmail.com>
2020-12-15 08:13:51 -0600

    Even Futher simplificaiton of memory game (day15)

--- aoc2020/day15.py
+++ aoc2020/day15.py
@@ -3,22 +3,17 @@ with open("day15.txt") as f_in:
     ins = f_in.read().strip()
 
 
-class Game:
-    def __init__(self, text):
-        data = [int(_) for _ in text.split(",")]
-        # Record the previous indexes -- except for the last element
-        # evaluated the last element
-        self.indexes = {val: idx for idx, val in enumerate(data[:-1])}
-        self.start = data[-1], len(data)
-
-    def item(self, index):
-        last, idx = self.start
-        while idx != index:
-            prev, self.indexes[last] = self.indexes.get(last), idx - 1
-            last = 0 if prev is None else (idx - prev - 1)
-            idx += 1
-        return last
+def game(text, index):
+    data = [int(_) for _ in text.split(",")]
+    # Record the previous indexes -- except for the last element
+    indexes = {val: idx for idx, val in enumerate(data[:-1])}
+    last, idx = data[-1], len(data)
+    while idx != index:
+        prev, indexes[last] = indexes.get(last), idx - 1
+        last = 0 if prev is None else (idx - prev - 1)
+        idx += 1
+    return last
 
 
-print(f"Result 1: {Game(ins).item(2020)}")
-print(f"Result 2: {Game(ins).item(30000000)}")
+print(f"Result 1: {game(ins,2020)}")
+print(f"Result 2: {game(ins,30000000)}")

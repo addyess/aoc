57e692751fa9c63f21ad0ed77d0be5261746a277
Adam Dyess <adyess@gmail.com>
2020-12-20 22:56:24 -0600

    Day20 p2

--- aoc2020/day20.py
+++ aoc2020/day20.py
@@ -1,6 +1,10 @@
 from math import sqrt, prod
+from itertools import cycle
+from functools import cached_property
 from collections import defaultdict, namedtuple
 import re
+
+Score = namedtuple('Score', 'tile, side, flipped, score')
 
 sample = False
 extension = ('' if not sample else '_sample')
@@ -9,140 +13,237 @@ with open(f"day20{extension}.txt") as f:
 tiles_data = text.split("\n\n")
 
 
+class Monster:
+    _SEA_MONSTER = """
+                  # 
+#    ##    ##    ###
+ #  #  #  #  #  #   """[1:].splitlines()
+
+    @classmethod
+    def craft(cls):
+        return cls({
+            (x, y)
+            for y, line in enumerate(cls._SEA_MONSTER)
+            for x, char in enumerate(line)
+            if char == "#"
+        }, len(cls._SEA_MONSTER[0]), len(cls._SEA_MONSTER))
+
+    def __init__(self, data, width, height):
+        self.data = data
+        self.width, self.height = width, height
+
+
 class Tile:
     _UUID = re.compile(r"Tile (\d+)")
+
+    @classmethod
+    def blank(cls, dim=10):
+        return cls(0, dim, {
+            (x, y): '.'
+            for y in range(dim)
+            for x in range(dim)
+        })
 
     @classmethod
     def parse(cls, tile_data):
         lines = [_.strip() for _ in tile_data.splitlines()]
         uuid, = cls._UUID.findall(lines[0])
-        return cls(int(uuid), {
+        return cls(int(uuid), len(lines[1:]), {
             (x, y): char
             for y, line in enumerate(lines[1:])
+            for x, char in enumerate(line)
+        })
+
+    @classmethod
+    def assembled(cls, lines):
+        return cls(0, len(lines), {
+            (x, y): char
+            for y, line in enumerate(lines)
             for x, char in enumerate(line)
         })
 
     def __repr__(self):
         return f"{self.uuid}"
 
-    @property
-    def lines(self):
-        title = f"Tile {self.uuid}: "
-        content = [
-            "".join(self._data[(x, y)] for x in range(0, 10)) + " "
-            for y in range(0, 10)
-        ]
-        return [title] + content + [" " * 11]
-
-    def __init__(self, uuid, data):
+    def __init__(self, uuid, dim, data):
         self.uuid = uuid
         self._data = data
+        self._dim = dim
+
+    def lines(self, borders=True):
+        title = f"Tile {self.uuid:04}: "
+        b = " " if borders else ""
+        s, e = (0, self._dim) if borders else (1, self._dim - 1)
+        content = [
+            "".join(self._data[(x, y)] for x in range(s, e)) + b
+            for y in range(s, e)
+        ]
+        if borders:
+            return [title] + content + [" " * (self._dim + 1)]
+        return content
+
+    def print(self, borders=True):
+        return "\n".join(self.lines(borders))
 
     def side(self, label, flip):
         def value(s):
             t = str.maketrans({'.': '0', '#': '1'})
             b = "".join(s).translate(t)
             return int(b[::-1] if flip else b, 2)
+
         prop = {
-            'l': (self._data[(0, y)] for y in range(10)),
-            'r': (self._data[(9, y)] for y in range(10)),
-            'b': (self._data[(x, 0)] for x in range(10)),
-            't': (self._data[(x, 9)] for x in range(10))
+            'l': (self._data[(0, y)] for y in range(self._dim - 1, -1, -1)),
+            'r': (self._data[(self._dim - 1, y)] for y in range(self._dim)),
+            'b': (self._data[(x, self._dim - 1)] for x in range(self._dim - 1, -1, -1)),
+            't': (self._data[(x, 0)] for x in range(self._dim))
         }
         return value(prop[label])
 
+    def scores(self):
+        for side in 'rltb':
+            for flip in (True, False):
+                score = self.side(side, flip)
+                yield Score(self, side, flip, score)
+
     def flip(self):
         """Flip Vertically"""
-        data = {(x, 9 - y): v for (x, y), v in self._data.items()}
-        return Tile(self.uuid, data)
+        data = {(x, self._dim - 1 - y): v for (x, y), v in self._data.items()}
+        return Tile(self.uuid, self._dim, data)
 
     def rr(self):
         """Rotate Right."""
-        data = {(9 - y, x): v for (x, y), v in self._data.items()}
-        return Tile(self.uuid, data)
+        data = {(self._dim - 1 - y, x): v for (x, y), v in self._data.items()}
+        return Tile(self.uuid, self._dim, data)
+
+    def mask(self, monster):
+        h, w = monster.height, monster.width
+        count = 0
+        for x in range(self._dim - w):
+            for y in range(self._dim - h):
+                locs = [(x + xm, y + ym) for xm, ym in monster.data ]
+                if all(self._data[_] == "#" for _ in locs):
+                    count += 1
+                    for _ in locs:
+                        self._data[_] = "O"
+        return count
+
+    def count(self, char="#"):
+        return sum(1 for _ in self._data.values() if _ == char)
 
 
 class Grid:
+    @staticmethod
+    def coord(idx, dim):
+        return idx % dim, idx // dim
+
     @classmethod
     def craft(cls, tiles):
         dim = int(sqrt(len(tiles)))
         return cls(dim, {
-            (idx % dim, idx // dim): tile
+            cls.coord(idx, dim): tile
             for idx, tile in enumerate(tiles)
+        })
+
+    @classmethod
+    def blank(cls, dim):
+        return cls(dim, {
+            cls.coord(idx, dim): Tile.blank()
+            for idx in range(dim * dim)
         })
 
     def __init__(self, dim, data):
         self.dim = dim
         self.data = data
 
-    @property
-    def lines(self):
+    def lines(self, border=True):
         return [
             "".join(_) + "\n"
             for y in range(self.dim)
-            for _ in zip(*(self.data[(x, y)].lines for x in range(self.dim)))
+            for _ in zip(*(
+                self.data[(x, y)].lines(border) for x in range(self.dim)
+            ))
         ]
 
-    def print(self, filename=None):
-        with open(filename, 'w') as fw:
-            fw.write('\033c')
-            fw.writelines(self.lines)
+    def exists(self, tile):
+        return any(t.uuid == tile.uuid for t in self.data.values())
 
-    def at(self, pos):
-        return self.data[pos]
+    def print(self, filename=None, border=True):
+        if filename:
+            with open(filename, 'w') as fw:
+                fw.write('\033c')
+                fw.writelines(self.lines(border))
+        return Tile.assembled(
+            [_.strip() for _ in self.lines(border=False)]
+        )
 
-    def flip(self, idx):
-        pos = (idx // self.dim, idx % self.dim)
-        self.data[pos] = self.data[pos].flip()
-        return self.data[pos]
+    def replace(self, idx, tile):
+        pos = self.coord(idx, self.dim)
+        self.data[pos] = tile
 
-    def rr(self, idx):
-        pos = (idx % self.dim, idx // self.dim)
-        self.data[pos] = self.data[pos].rr()
-        return self.data[pos]
+    @cached_property
+    def scores(self):
+        scores = defaultdict(list)
+        for tile in self.data.values():
+            for score in tile.scores():
+                scores[score.score].append(score)
+        return scores
 
-    def swap(self, idx1, idx2):
-        data = dict(self.data.items())
-        pos1 = (idx1 % self.dim, idx1 // self.dim)
-        pos2 = (idx2 % self.dim, idx2 // self.dim)
-        data[pos2], data[pos1] = data[pos1], data[pos2]
-        return Grid(self.dim, data)
+    @cached_property
+    def edges(self):
+        edges = defaultdict(list)
+        for score, pieces in self.scores.items():
+            if len(pieces) == 1:
+                edges[pieces[0].tile].append(pieces[0])
+        return edges
 
+    @cached_property
     def corners(self):
-        pos = [(0, 0), (0, self.dim - 1), (self.dim - 1, 0), (self.dim - 1, self.dim - 1)]
-        return [self.data[_] for _ in pos]
+        return dict((t, l) for t, l in self.edges.items() if len(l) == 4)
 
-    def neighbors(self, x, y):
-        pos = [(x - 1, y, "lr"), (x + 1, y, "rl"), (x, y - 1, "tb"), (x, y + 1, "bt")]
-        return [(self.data[(x, y)], r) for x, y, r in pos if (x, y) in self.data]
+    def neighbor(self, tile, dir):
+        score = next(
+            t for t in tile.scores() if not t.flipped and t.side == dir
+        )
+        pair = self.scores[score.score]
+        return next(p for p in pair if p.tile.uuid != tile.uuid)
 
-    @property
-    def alignment(self):
-        for y in range(0, self.dim):
-            for x in range(0, self.dim):
-                my = self.data[(x, y)]
-                for tile, rel in self.neighbors(x, y):
-                    if not all(
-                            p1 == p2
-                            for p1, p2 in zip(my.line(rel[0]), tile.line(rel[1]))
-                    ):
-                        return False
+    def fill(self):
+        to_fill = Grid.blank(self.dim)
+        insert = next(
+            t for t, tile_scores in self.corners.items()
+            if all(_.side in 'lt' for _ in tile_scores)
+        )
+        snake = cycle(("rl", "bt", "lr", "bt"))
+        slot, inc = 0, 1
+        for idx in range(self.dim * self.dim):
+            to_fill.replace(slot, insert)
+            if idx % self.dim == 0:
+                c, n = tuple(next(snake))
+            elif idx == (self.dim * self.dim) - 1:
+                break
+            elif idx % self.dim == self.dim - 1:
+                c, n = tuple(next(snake))
+                slot = (idx + self.dim + 1) if inc > 0 else idx
+                inc = 0 - inc
+            next_score = self.neighbor(insert, c)
+            next_tile = next_score.tile
+            if not next_score.flipped:
+                next_tile = next_tile.flip()
+            while next_tile.side(n, True) != next_score.score:
+                next_tile = next_tile.rr()
+            insert = next_tile
+            slot += inc
+        return to_fill
 
 
-grid = Grid.craft([Tile.parse(_) for _ in tiles_data])
+piece_grid = Grid.craft([Tile.parse(_) for _ in tiles_data])
 LOG_FILE = f"day20{extension}.log"
-scores = defaultdict(list)
-Score = namedtuple('Score', 'tile, flipped')
+print(f"Result 1: {prod(s.uuid for s in piece_grid.corners.keys())}")
 
-for tile in grid.data.values():
-    for side in 'rltb':
-        for flip in (True, False):
-            s = tile.side(side, flip)
-            scores[s].append(Score(tile, flip))
+monster = Monster.craft()
+sat_image = piece_grid.fill()
+sat_tile = sat_image.print(LOG_FILE, border=False)
+while sat_tile.mask(monster) == 0:
+    sat_tile = sat_tile.rr()
+print(f"Result 2: {sat_tile.count()}")
 
-single_sided = defaultdict(list)
-for score, pieces in scores.items():
-    if len(pieces) == 1:
-        single_sided[pieces[0].tile].append(score)
-
-print(f"Result 1: {prod(s.uuid for s,l in single_sided.items() if len(l)==4)}")

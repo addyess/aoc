c35fd0ffda2b28cc48ea0d6967fc9c9a79c7e001
Adam Dyess <adam.dyess@canonical.com>
2021-12-02 09:00:18 -0600

    Day 02, 2021

---
+++ aoc2021/day02.py
@@ -0,0 +1,37 @@
+#! /usr/bin/python3
+from functools import reduce
+
+
+def parse(line):
+    command, scalar = line.split()
+    return command, int(scalar)
+
+
+def navigate(loc, cur):
+    inst, value = cur
+    if inst == 'forward':
+        loc[0] += value
+    else:
+        loc[1] += value if inst == 'down' else (0 - value)
+    return loc
+
+
+def navigate_by_aim(acc, cur):
+    inst, value = cur
+    loc, aim = acc
+    if inst == 'forward':
+        loc[0] += value
+        loc[1] += aim * value
+    else:
+        aim += value if inst == 'down' else (0 - value)
+    return loc, aim
+
+
+with open("day02.txt") as f_in:
+    ins = list(map(parse, f_in))
+
+end = reduce(navigate, ins, [0, 0])
+print(f"Result 1: {end[0]*end[1]}")
+
+end, _ = reduce(navigate_by_aim, ins, ([0, 0], 0))
+print(f"Result 2: {end[0]*end[1]}")

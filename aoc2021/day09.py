29b9074bb457703a87eb53afef42234547bcc49f
Adam Dyess <adam.dyess@canonical.com>
2021-12-09 14:43:39 -0600

    golf day09, 2021

--- aoc2021/day09.py
+++ aoc2021/day09.py
@@ -1,22 +1,20 @@
 from collections import defaultdict
-from dataclasses import dataclass
-from functools import cached_property
 import math
 
 
-@dataclass
 class Cave:
-    height_map: dict[tuple, int]
-
-    @classmethod
-    def parse(cls, lines):
-        return cls(
-            {
-                (x, y): int(height)
-                for x, line in enumerate(lines)
-                for y, height in enumerate(line.strip())
-            }
-        )
+    def __init__(self, lines):
+        self.map = {
+            (x, y): int(height)
+            for x, line in enumerate(lines)
+            for y, height in enumerate(line.strip())
+        }
+        self.inf_boundary_map = defaultdict(lambda: float("inf"), self.map.items())
+        self.low_points = {
+            loc: height + 1
+            for loc, height in self.map.items()
+            if height < min(self.inf_boundary_map[a] for a in self.adjacent(loc))
+        }
 
     @staticmethod
     def adjacent(pos):
@@ -24,38 +22,20 @@ class Cave:
         for x, y in [(0, -1), (0, 1), (-1, 0), (1, 0)]:
             yield pos_x + x, pos_y + y
 
-    @cached_property
-    def inf_boundary_map(self):
-        return defaultdict(lambda: float("inf"), self.height_map.items())
+    def adjacent_members_of_basin(self, loc, basin_set=None):
+        basin_set = (basin_set or set()) | {loc}
+        for a in self.adjacent(loc):
+            if a not in basin_set and self.inf_boundary_map[a] < 9:
+                basin_set |= self.adjacent_members_of_basin(a, basin_set)
+        return basin_set
 
-    @cached_property
-    def low_points(self):
-        """Finds the lowest local points, and returns the score at those points."""
-        return {
-            loc: height + 1
-            for loc, height in self.height_map.items()
-            if height < min(self.inf_boundary_map[a] for a in self.adjacent(loc))
-        }
-
-    @cached_property
+    @property
     def basins(self):
-        """
-        Finds each basin lower than height 9 centered on their low-point, and provides
-        the coordinates to each point in the basin
-        """
-
-        def adjacent_members_of_basin(loc, basin_set=None):
-            basin_set = (basin_set or set()) | {loc}
-            for a in self.adjacent(loc):
-                if a not in basin_set and self.inf_boundary_map[a] < 9:
-                    basin_set |= adjacent_members_of_basin(a, basin_set)
-            return basin_set
-
-        return {lp: len(adjacent_members_of_basin(lp)) for lp in self.low_points}
+        return {lp: len(self.adjacent_members_of_basin(lp)) for lp in self.low_points}
 
 
 with open("day09.txt") as fin:
-    cave = Cave.parse(fin)
+    cave = Cave(fin)
 res1 = sum(cave.low_points.values())
 print(f"Result 1: {res1}")
 largest_basins = sorted(cave.basins.values(), reverse=True)
